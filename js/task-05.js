b860af1dab1a152305434ca852b905a40051d90a
kovalchuktaisiia <kovalchuk.taisiia@google.com>
2021-04-30 10:21:01 +0300

    5

--- js/task-05.js
+++ js/task-05.js
@@ -1,4 +1,31 @@
-// Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input), подставляет его текущее значение в span#name-output. Если инпут пустой, в спане должна отображаться строка 'незнакомец'.
+// Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input), 
+//подставляет его текущее значение в span#name-output. 
+//Если инпут пустой, в спане должна отображаться строка 'незнакомец'.
 
-// <input type="text" placeholder="Ваше имя?" id="name-input" />
-// <h1>Привет, <span id="name-output">незнакомец</span>!</h1>
+//  <input type="text" placeholder="Ваше имя?" id="name-input" />
+//  <h1>Привет, <span id="name-output">незнакомец</span>!</h1>
+
+
+const refs = {
+    input: document.querySelector('input'),
+    nameOutput: document.querySelector('h1 > span'),
+ };
+   console.log(refs);
+
+  //  refs.input.addEventListener('focus', onInputFocus);
+  //  refs.input.addEventListener('blur', onInputBlur);
+ 
+  function onInputFocus() {
+    console.log('Инпут получил фокус - событие focus');
+  }
+  
+  function onInputBlur() {
+    console.log('Инпут потерял фокус - событие blur');
+  }
+
+  refs.input.addEventListener('input', onInputChange);
+
+  function onInputChange(event) {
+    refs.nameOutput.textContent = event.currentTarget.value;
+  }
+  
