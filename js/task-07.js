d6b849fe64c92e5892181d35f459618c7c0ef3b0
kovalchuktaisiia <kovalchuk.taisiia@google.com>
2021-04-30 13:41:58 +0300

    4

--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,20 @@
-// Напиши скрипт, который реагирует на изменение значения input#font-size-control (событие input) и изменяет инлайн-стиль span#text обновляя свойство font-size. В результате при перетаскивании ползунка будет меняться размер текста.
+// Напиши скрипт, который реагирует на 
+// изменение значения input#font-size-control (событие input) 
+// и изменяет инлайн-стиль span#text обновляя свойство font-size. 
+// В результате при перетаскивании ползунка будет меняться размер текста.
 
-// <input id="font-size-control" type="range" />
-// <br />
-// <span id="text">Абракадабра!</span>
+{/* <input id="font-size-control" type="range" />
+<br />
+<span id="text">Абракадабра!</span> */}
+
+const refs = {
+    input: document.querySelector('#font-size-control'),
+    text: document.querySelector('#text')  
+ };
+ 
+    refs.input.addEventListener('input', onControlerInput);
+    
+  function onControlerInput(event) {
+    console.log('Инпут получил фокус - событие focus');
+    text.style.fontSize = event.currentTarget.value + "px";
+  }
